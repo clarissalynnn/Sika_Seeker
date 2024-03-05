29147800ea81c95ed6ad7686369b73fb395d9fcc
Jason Roche <jason.p.roche@hotmail.com>
2024-03-05 18:16:27 +0800

    add items, add item images to assets/images

---
+++ db/ITEMS.rb
@@ -0,0 +1,214 @@
+module Items
+  RICE = [
+    {
+      name: "Nasi putih",
+      description: "White rice",
+      photo_url: "https://example.com/photo1.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Nasi merah",
+      description: "Red rice",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Nasi Kuning",
+      description: "Yellow rice",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "10.000"
+    },
+  ]
+
+
+  ANIMAL_PROTEIN = [
+    {
+      name: "Babi Kecap",
+      description: "Braised pork",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "16.000"
+    },
+    {
+      name: "Sarden",
+      description: "Sardines in tomato sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "15.000"
+    },
+    {
+      name: "Ikan saos cabe",
+      description: "Fish in spicy sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "16.000"
+    },
+    {
+      name: "Ayam sambal matah",
+      description: "Chicken in Balinese sambal",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "16.000"
+    },
+    {
+      name: "Ayam suwir",
+      description: " Spicy shredded chicken",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "15.000"
+    },
+    {
+      name: "Ayam goreng",
+      description: "Fried chicken",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "17.000"
+    },
+    {
+      name: "Ayam bakar",
+      description: "Grilled chicken",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "18.000"
+    },
+    {
+      name: "Ayam Asam Manis",
+      description: "Sweet and sour chicken",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "17.000"
+    },
+    {
+      name: "Ayam Rendang",
+      description: "Chicken Rendang",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "18.000"
+    },
+    {
+      name: "Ayam Gulai",
+      description: "Chicken in yellow coconut sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "18.000"
+    },
+    {
+      name: "Babi goreng",
+      description: "Fried pork",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "16.000"
+    },
+    {
+      name: "Sate ayam",
+      description: "Chicken satay",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Telor kecap",
+      description: "Braised egg",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Telor mata sapi",
+      description: "Sunny side up",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: " Telor balado",
+      description: "Egg in sweet chili sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Telor dadar",
+      description: "Fried egg",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "8.000"
+    },
+    {
+      name: "Hati Ayam Kecap",
+      description: "Braised liver & gizzard",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "7.000"
+    },
+    {
+      name: "Kulit ayam goreng",
+      description: "Fried chicken skin",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "5.000"
+    },
+  ]
+
+
+  PLANT_PROTEIN = [
+    {
+      name: "Tempe tahu gulai",
+      description: "Tempeh and tofu in yellow coconut sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "7.000"
+    },
+    {
+      name: "Tempe manis garing",
+      description: "Crispy sweet tempeh",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tempe manis",
+      description: "Sweet tempeh",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tahu asem manis",
+      description: "Sweet and sour tofu",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tempe bakar",
+      description: "Grilled tempeh",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tempe goreng",
+      description: "Fried tempeh",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tahu goreng",
+      description: "Fried tofu",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "5.000"
+    },
+  ]
+
+
+  VEGETABLES = [
+    {
+      name: "Mie goreng",
+      description: "Fried noodle",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Sayur tumis bawang putih",
+      description: "Assorted vegetable garlic stirfry",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tumis bayam dan tauge",
+      description: "Sauteed spinach and beansprouts",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Tumis kangkung",
+      description: "Sauteed water spinach",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "6.000"
+    },
+    {
+      name: "Singkong gulai",
+      description: "Cassava leaves in coconut sauce",
+      photo_url: "https://example.com/photo2.jpg",
+      price: "7.000"
+    },
+  ]
+end
