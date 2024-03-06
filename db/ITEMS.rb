508eb83f6302db75b7fa94bbfe462532e89a8efc
Jason Roche <jason.p.roche@hotmail.com>
2024-03-06 11:22:46 +0800

    add images to items

--- db/ITEMS.rb
+++ db/ITEMS.rb
@@ -3,19 +3,19 @@ module Items
     {
       name: "Nasi putih",
       description: "White rice",
-      photo_url: "https://example.com/photo1.jpg",
+      photo_url: "white_rice.png",
       price: "8.000"
     },
     {
       name: "Nasi merah",
       description: "Red rice",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "red_rice.png",
       price: "8.000"
     },
     {
       name: "Nasi Kuning",
       description: "Yellow rice",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "yellow_rice.png",
       price: "10.000"
     },
   ]
@@ -25,109 +25,109 @@ module Items
     {
       name: "Babi Kecap",
       description: "Braised pork",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "braised_pork.png",
       price: "16.000"
     },
     {
       name: "Sarden",
       description: "Sardines in tomato sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sarden.png",
       price: "15.000"
     },
     {
       name: "Ikan saos cabe",
       description: "Fish in spicy sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fish_spicy.png",
       price: "16.000"
     },
     {
       name: "Ayam sambal matah",
       description: "Chicken in Balinese sambal",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "chicken_bali_sambal.png",
       price: "16.000"
     },
     {
       name: "Ayam suwir",
       description: " Spicy shredded chicken",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "spicy_pulled_chicken.png",
       price: "15.000"
     },
     {
       name: "Ayam goreng",
       description: "Fried chicken",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fried_chicken.png",
       price: "17.000"
     },
     {
       name: "Ayam bakar",
       description: "Grilled chicken",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "grilled_chicken.png",
       price: "18.000"
     },
     {
       name: "Ayam Asam Manis",
       description: "Sweet and sour chicken",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sweet_sour_chicken.png",
       price: "17.000"
     },
     {
       name: "Ayam Rendang",
       description: "Chicken Rendang",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "chicken_rendang.png",
       price: "18.000"
     },
     {
       name: "Ayam Gulai",
       description: "Chicken in yellow coconut sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "ayam_gulai.png",
       price: "18.000"
     },
     {
       name: "Babi goreng",
       description: "Fried pork",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fried_pork.png",
       price: "16.000"
     },
     {
       name: "Sate ayam",
       description: "Chicken satay",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sate_ayam.png",
       price: "8.000"
     },
     {
       name: "Telor kecap",
       description: "Braised egg",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "braised_egg.png",
       price: "8.000"
     },
     {
       name: "Telor mata sapi",
       description: "Sunny side up",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sun_side_up.png",
       price: "8.000"
     },
     {
       name: " Telor balado",
       description: "Egg in sweet chili sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "telor_balado.png",
       price: "8.000"
     },
     {
       name: "Telor dadar",
       description: "Fried egg",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "telor_dadar.png",
       price: "8.000"
     },
     {
       name: "Hati Ayam Kecap",
       description: "Braised liver & gizzard",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "braised_liver.png",
       price: "7.000"
     },
     {
       name: "Kulit ayam goreng",
       description: "Fried chicken skin",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fried_chicken_skin.png",
       price: "5.000"
     },
   ]
@@ -137,43 +137,43 @@ module Items
     {
       name: "Tempe tahu gulai",
       description: "Tempeh and tofu in yellow coconut sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "tempe_tahu_gulai.png",
       price: "7.000"
     },
     {
       name: "Tempe manis garing",
       description: "Crispy sweet tempeh",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "crispy_sweet_tempeh.png",
       price: "6.000"
     },
     {
       name: "Tempe manis",
       description: "Sweet tempeh",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sweet_tempeh.png",
       price: "6.000"
     },
     {
       name: "Tahu asem manis",
       description: "Sweet and sour tofu",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sweet_sour_tofu.png",
       price: "6.000"
     },
     {
       name: "Tempe bakar",
       description: "Grilled tempeh",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "grilled_tempeh.png",
       price: "6.000"
     },
     {
       name: "Tempe goreng",
       description: "Fried tempeh",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fried_tempeh.png",
       price: "6.000"
     },
     {
       name: "Tahu goreng",
       description: "Fried tofu",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "fried_tofu.png",
       price: "5.000"
     },
   ]
@@ -183,31 +183,31 @@ module Items
     {
       name: "Mie goreng",
       description: "Fried noodle",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "mie_goreng.png",
       price: "6.000"
     },
     {
       name: "Sayur tumis bawang putih",
       description: "Assorted vegetable garlic stirfry",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "assorted_vegetable_garlic_stirfry.png",
       price: "6.000"
     },
     {
       name: "Tumis bayam dan tauge",
       description: "Sauteed spinach and beansprouts",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sauteed_spinach_beansprouts.png",
       price: "6.000"
     },
     {
       name: "Tumis kangkung",
       description: "Sauteed water spinach",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "sauteed_water_spinach.png",
       price: "6.000"
     },
     {
       name: "Singkong gulai",
       description: "Cassava leaves in coconut sauce",
-      photo_url: "https://example.com/photo2.jpg",
+      photo_url: "singkong_gulai.png",
       price: "7.000"
     },
   ]
