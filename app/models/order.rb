25d6092d2944ca568cbe51cfa6379512ad8c72c4
Jason Roche <jason.p.roche@hotmail.com>
2024-03-07 16:49:41 +0800

    update models to accomodate driver

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :customer, class_name: "User", foreign_key: "customer_id"
-  belongs_to :driver, class_name: "User", foreign_key: "driver_id"
+  belongs_to :driver, -> { where(is_driver: true) }, class_name: "User", foreign_key: "driver_id"
   has_many :order_items
+
+  validate :driver_must_be_driver
+
+  private
+
+  def driver_must_be_driver
+    errors.add(:driver, "must be a driver") unless driver&.is_driver?
+  end
 end
