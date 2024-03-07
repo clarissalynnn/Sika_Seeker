1f2c6c4dc2d75a045736d6667a19bffd12e5b6a0
Jason Roche <jason.p.roche@hotmail.com>
2024-03-07 16:44:09 +0800

    change redirect for driver after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   # Redirect after sign in
-  def after_sign_in_path_for(*)
-    items_index_path
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.is_driver?
+      orders_path
+    else
+      items_index_path
+    end
   end
 end
